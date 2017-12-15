1795ac043ee02bac47cbdaeced47ef2d67963225
HelicopterHig <32367339+HelicopterHig@users.noreply.github.com>
2017-12-15 15:25:30 +0300

    Update tuiring.c

--- tuiring.c
+++ tuiring.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct charlist{
+    unsigned char compound;
+    struct charlist *next;
+    struct charlist *early;
+} charlist;
+
+typedef struct listline{
+    struct charlist *head;
+    struct charlist *tail;
+    int len;
+} listline;
